5b9a17b95e16d298ab1823293bf8a3c52e711e44
Andrew Mass <amass1212@gmail.com>
2014-01-04 23:11:38 -0600

    Reorganize tiles. Update styling. Add highlight feature.

--- js/index.js
+++ js/index.js
@@ -13,7 +13,21 @@ MassWeb.verticalCenter = function() {
   var containerHeight = $('.container').outerHeight();
   var screenHeight = $(window).innerHeight();
   var margin = (screenHeight - containerHeight) / 2;
-  $('.container').css('margin-top', margin);
+  if(margin >= 0) {
+    $('.container').css('margin-top', margin);
+  }
+  else {
+    $('.container').css('margin-top', 10);
+  }
+};
+
+MassWeb.highlightCategory = function(args) {
+  if(args === '') {
+    $('td').removeClass('hover');
+  } else {
+    $('td').removeClass('hover');
+    $('td' + args).addClass('hover');
+  }
 };
 
 $(function() {
@@ -21,5 +35,21 @@ $(function() {
 
   $(window).resize(function() {
     MassWeb.verticalCenter();
+  });
+
+  $('#btn-social').click(function() {
+    MassWeb.highlightCategory('.social');
+  });
+
+  $('#btn-dev').click(function() {
+    MassWeb.highlightCategory('.dev');
+  });
+
+  $('#btn-exp').click(function() {
+    MassWeb.highlightCategory('.exp');
+  });
+
+  $('#btn-clear').click(function() {
+    MassWeb.highlightCategory('');
   });
 });
