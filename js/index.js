2f0370192a7e988ebf0aaf7cc33ea7d9951f0a5a
Andrew Mass <amass1212@gmail.com>
2014-01-05 22:36:41 -0600

    Improve category highlight and click functions

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@
 
 function MassWeb() {}
 
+MassWeb.socClicked = false;
+MassWeb.devClicked = false;
+MassWeb.expClicked = false;
+
 MassWeb.track = function(args) {
   _gaq.push(args); // MassWeb.track(['_trackEvent', 'Start', 'Start-Header']);
 };
@@ -30,6 +34,10 @@ MassWeb.highlightCategory = function(args) {
   }
 };
 
+MassWeb.anyClicked = function() {
+  return (this.socClicked || this.devClicked || this.expClicked);
+}
+
 $(function() {
   MassWeb.verticalCenter();
 
@@ -37,19 +45,55 @@ $(function() {
     MassWeb.verticalCenter();
   });
 
-  $('#btn-social').click(function() {
-    MassWeb.highlightCategory('.social');
+  $('#btn-soc').click(function() {
+    MassWeb.highlightCategory('.soc');
+    MassWeb.socClicked = !MassWeb.socClicked;
+  });
+
+  $('#btn-soc').hover(function() {
+    if(!MassWeb.anyClicked()) {
+      MassWeb.highlightCategory('.soc'); 
+    }
+  }, function() {
+    if(!MassWeb.anyClicked()) {
+      MassWeb.highlightCategory('');
+    }
   });
 
   $('#btn-dev').click(function() {
     MassWeb.highlightCategory('.dev');
+    MassWeb.devClicked = !MassWeb.devClicked;
+  });
+
+  $('#btn-dev').hover(function() {
+    if(!MassWeb.anyClicked()) {
+      MassWeb.highlightCategory('.dev'); 
+    }
+  }, function() {
+    if(!MassWeb.anyClicked()) {
+      MassWeb.highlightCategory('');
+    }
   });
 
   $('#btn-exp').click(function() {
     MassWeb.highlightCategory('.exp');
+    MassWeb.expClicked = !MassWeb.expClicked;
+  });
+
+  $('#btn-exp').hover(function() {
+    if(!MassWeb.anyClicked()) {
+      MassWeb.highlightCategory('.exp'); 
+    }
+  }, function() {
+    if(!MassWeb.anyClicked()) {
+      MassWeb.highlightCategory('');
+    }
   });
 
   $('#btn-clear').click(function() {
     MassWeb.highlightCategory('');
+    MassWeb.socClicked = false;
+    MassWeb.devClicked = false;
+    MassWeb.expClicked = false;
   });
 });
